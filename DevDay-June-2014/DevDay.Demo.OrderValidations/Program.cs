c63297d08a33e5e9761a9210f999dcdceede6134
denisivan0v <ivanoff.denis@gmail.com>
2014-06-23 00:27:26 +0700

    initial commit

---
+++ DevDay-June-2014/DevDay.Demo.OrderValidations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+
+using Owin;
+
+namespace DevDay.Demo.OrderValidations
+{
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed. Suppression is OK here.")]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            const string Url = "http://localhost:8081";
+            using (WebApp.Start(Url))
+            {
+                Console.WriteLine("Server running on {0}", Url);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed. Suppression is OK here.")]
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed. Suppression is OK here.")]
+    public class SignalRHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+    }
+}
